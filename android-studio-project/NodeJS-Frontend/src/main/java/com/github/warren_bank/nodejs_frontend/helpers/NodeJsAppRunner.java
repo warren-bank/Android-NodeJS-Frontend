c5d67b8d04d81e9da9b07b569e5ca26d7be95463
Warren R. Bank <warren.r.bank@gmail.com>
2021-02-12 03:24:16 -0800

    add Node.js binaries, enable feature to "exec" in same process
    incomplete implementation: does not yet capture stdout

--- android-studio-project/NodeJS-Frontend/src/main/java/com/github/warren_bank/nodejs_frontend/helpers/NodeJsAppRunner.java
+++ android-studio-project/NodeJS-Frontend/src/main/java/com/github/warren_bank/nodejs_frontend/helpers/NodeJsAppRunner.java
@@ -6,10 +6,53 @@ import android.content.Context;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public final class NodeJsAppRunner {
 
+  static {
+    System.loadLibrary("native-lib");
+    System.loadLibrary("node");
+  }
+
+  private static native Integer startNodeWithArguments(String[] arguments);
+  private static native Integer setenv(String name, String value, Integer overwrite);
+
   public static void exec(NodeJsApp listItem) {
+    String[][] env_vars     = listItem.getEnvironmentVariables();
+    String[]   node_options = listItem.getNodeOptions();
+    String     js_filepath  = listItem.getJsApplicationFilepath();
+    String[]   js_options   = listItem.getJsApplicationOptions();
+
+    ArrayList<String> arguments = new ArrayList<String>();
+
+    if (env_vars != null) {
+      for (int i = 0; i < env_vars.length; i++) {
+        if (env_vars[i].length == 2) {
+          setenv(env_vars[i][0], env_vars[i][1], 1);
+        }
+      }
+    }
+
+    arguments.add("node");
+
+    if (node_options != null) {
+      Collections.addAll(arguments, node_options);
+    }
+
+    if (!js_filepath.isEmpty()) {
+      arguments.add(js_filepath);
+
+      if (js_options != null) {
+        Collections.addAll(arguments, js_options);
+      }
+    }
+
+    if (arguments.size() > 1) {
+      startNodeWithArguments(
+        arguments.toArray(new String[ arguments.size() ])
+      );
+    }
   }
 
   public static File getStandardOutputFile(Context context, String id, boolean mustExist) {
